0739837f82efcfc3c6d5081a32c862beb4983f93
sadeem sajid <stormshadow2001@gmail.com>
2020-11-19 19:01:30 +0500

    Linked Logo

--- Main/main_index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("Button").onclick = function() {
-    var myText = document.getElementById("inputBox").value;
-    document.getElementById("h2Tag").innerHTML = 'Your text is: ' + myText;
-};
